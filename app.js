96f32a8c74708905809eff54d88e8f59ec7fdb23
Bokeefe <brndnokf@yahoo.com>
2017-01-10 06:21:07 -0700

    first

---
+++ app.js
@@ -0,0 +1,40 @@
+/* jslint esversion:6*/
+
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var yea = 0;
+var nay = 0;
+var connectCounter = 0;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connect', function() { connectCounter++; });
+io.on('disconnect', function() { connectCounter--; });
+
+io.on('connection', (socket) => {
+        io.sockets.emit('update-msg', { data: connectCounter});
+        socket.on('disconnect', () => {
+            console.log("somebody disconnected" + connectCounter);
+            io.sockets.emit('update-msg', { data: connectCounter});
+});
+
+
+
+        socket.on('button click', (message) => {
+            yea += 1;
+        io.emit('recv click', yea);
+        });
+        socket.on('button click2', (message) => {
+            nay += 1;
+        io.emit('recv click2', nay);
+        });
+        
+});
+io.emit('count', connectCounter);
+
+http.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
