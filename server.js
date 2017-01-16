f2c2760444d3dfbe85e4ef27552020e23af97cf6
Bokeefe <brndnokf@yahoo.com>
2017-01-16 09:24:36 -0700

    fixing .gitignore

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var yeaP = 0;
 var nayP = 0;
 var connectCounter = 0;
 
+
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -17,6 +18,29 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+
+app.get('/votepush', (req, res) => {
+ res.sendFile(__dirname + '/example.json');
+});
+
+app.post('/voteSend', (req, res) => { 
+    console.log(req.body.votes);
+    console.log(appendObject(req.body.votes));
+    fs.writeFile('./example.json', req.body.votes, (err) => {
+        console.log(req.body.votes);
+        if (err) throw err;
+            res.send("add Vote");
+    });
+});
+
+function appendObject(obj){
+  var configFile = fs.readFileSync('./example.json');
+  var config = JSON.parse(configFile);
+  config.push(obj);
+  var configJSON = JSON.stringify(config);
+  fs.writeFileSync('./example.json', configJSON);
+}
 
 io.on('connect', function() { connectCounter++; });
 io.on('disconnect', function() { connectCounter--; });
@@ -50,10 +74,7 @@ function update(){
 }
 var timer = setInterval(update,1500);
 
-http.listen(3000, () => { /////local host
-    console.log("Server started on port 3000");
-});
 
 
-// http.listen(3000, '104.131.1.90');
-// console.log('Server running at http://104.131.1.90:3000/');
+http.listen(3000, '104.131.1.90');
+console.log('Server running at http://104.131.1.90:3000/');
