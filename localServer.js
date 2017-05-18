d9803751fadb069d860dd8e992391b6449a85955
Brendan O'Keefe <brndnokf@yahoo.com>
2017-05-18 13:32:36 -0600

    stuck not creating rooms

--- localServer.js
+++ localServer.js
@@ -2,32 +2,16 @@
 var fs = require("fs");
 var express = require('express');
 var session = require('express-session');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = require("express")();
-var Schema = mongoose.Schema;
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
-var User = require('./VoteSchema.js')(mongoose);
 var io = require("socket.io")(http);
 var PORT = 3000;
 
-var yea = 0;
-var nay = 0;
-var yeaP = 0;
-var nayP = 0;
-var connectCounter = 0;
 
-mongoose.connect('mongodb://localhost');
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-});
-
-// var User = mongoose.model('User', UserSchema);
-
-
+var allVotes = [];
+var voteID = 0;
+var currentVotes = [];
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -37,77 +21,112 @@ app.get("/", (req, res) => {
 });
 
 
-app.get('/votepush', (req, res) => {
- res.sendFile(__dirname + '/example.json');
+
+
+app.post('/newVote', (req, res) => {
+    var newVote = JSON.parse(req.body.voteName);
+    session.voteName = newVote;
+    allVotes.push(newVote);
+    session.allVotes = allVotes;
+    newVote = {"connectCounter":0, "yea":0, "nay":0};
+    currentVotes.push(newVote);
+    session.currentVotes = currentVotes;
+    res.send(session.voteName);
+
+});
+app.get("/getSession", (req, res) => {
+    res.send(session.allVotes);
 });
 
-app.post('/voteSend', (req, res) => { 
-    var store = [];
-    var last = 0;
-    var hold = req.body.votes;
-    
-    hold = JSON.parse(hold);
-    for(var i in hold){
-        last = hold.length;
-    }
-    for(i in hold){
-        store.push(hold[i]);
-    }
-    store = {"Name": hold[last-1].name,"Time":hold[last-1].time,"Yea":  yeaP, "Nay": nayP, "total": connectCounter};
-    var lastPoint = hold.pop(hold[last]);
-    var lastPoint2 = {"Name": lastPoint.name,"Time":lastPoint.time,"Yea":  yeaP, "Nay": nayP, "total": connectCounter};
-    hold  = hold.concat(lastPoint2);//////////dont erase until you get WTF
-    hold = JSON.stringify(hold);
-    //console.log(hold);
-    fs.writeFile('./example.json', hold, (err) => {
-        if (err) throw err;
-            res.send("add Vote");
+var voteID = 0;
+
+app.post("/votePick", (req, res) => {
+        voteID = req.body.votePick;
+        res.send(req.body.votePick);
+        var nsp = io.of('/'+voteID);
+
+     app.get("/"+voteID, (req, res) => {
+        res.sendFile(__dirname + "/public/canvas.html");
     });
-    server.close();
-});
 
-io.on('connect', function() { connectCounter++; });
-io.on('disconnect', function() { 
-    connectCounter--;
-    yea = 0;
-    nay = 0;
-    yeaP = 0;
-    nayP = 0;
-    console.log("blamm");
-     });
 
-io.on('connection', (socket) => {
-        io.sockets.emit('update-msg', { data: connectCounter});
-        socket.on('disconnect', () => {
-            connectCounter--;
-            io.sockets.emit('update-msg', { data: connectCounter});
-});
 
-socket.on('button click', (message) => {
-        yea += 1;
-        yeaP = Math.floor(yea/connectCounter*100);
-    io.emit('recv click', yea);
+    app.get('/votepush', (req, res) => {
+     res.sendFile(__dirname + '/example.json');
     });
-    socket.on('button click2', (message) => {
-        nay += 1;
-        nayP = Math.floor(nay/connectCounter*100);
-    io.emit('recv click2', nay);
-    });
-        
-});
-io.emit('count', connectCounter);
 
-function update(){
-    io.emit('recv click', yea);
-    io.emit('recv click2', nay);
-    io.emit('recv percY', Math.floor(yea/connectCounter*100));
-    io.emit('recv percN', Math.floor(nay/connectCounter*100));
+    app.post('/voteSend', (req, res) => {
+        var store = [];
+        var last = 0;
+        var hold = req.body.votes;
+
+        hold = JSON.parse(hold);
+        for(var i in hold){
+            last = hold.length;
+        }
+        for(i in hold){
+            store.push(hold[i]);
+        }
+        store = {"Name": hold[last-1].name,"Time":hold[last-1].time,"Yea":  yeaP, "Nay": nayP, "total": connectCounter};
+        var lastPoint = hold.pop(hold[last]);
+        var lastPoint2 = {"Name": lastPoint.name,"Time":lastPoint.time,"Yea":  yeaP, "Nay": nayP, "total": connectCounter};
+        hold  = hold.concat(lastPoint2);//////////dont erase until you get WTF
+        hold = JSON.stringify(hold);
+        fs.writeFile('./example.json', hold, (err) => {
+            if (err) throw err;
+                res.send("add Vote");
+        });
+        server.close();
+    });
+
+
+
+//////////////SOCKET STUFF///////
+    nsp.on('connection', function(socket){
+    currentVotes[voteID].connectCounter++;
+    nsp.emit('update-msg',currentVotes[voteID]);
+      console.log('a user connected to '+voteID+" count "+ JSON.stringify(currentVotes[voteID]));
+        nsp.emit('update-msg',currentVotes[voteID]);
+        socket.on('button yea', function (data) {
+            console.log(data+" data");
+            currentVotes[voteID].yea++;
+            console.log(currentVotes[voteID]);
+            io.emit('update-msg',currentVotes[voteID]);
+            update(currentVotes[voteID]);
+        });
+        socket.on('button nay', function (data) {
+            currentVotes[voteID].nay++;
+            console.log(data);
+            console.log(currentVotes[voteID]);
+          io.emit('update-msg',currentVotes[voteID]);
+              update(currentVotes[voteID]);
+        });
+
+
+
+
+        socket.on('disconnect', function(){
+        nsp.emit('update-msg',currentVotes[voteID]);
+            // currentVotes[voteID].connectCounter--;
+            // console.log('user disconnected from '+currentVotes[voteID].connectCounter);
+
+        });
+
+
+
+    });
+/////////END SOCKET///////////
+
+
+function update(data){
+    nsp.emit('update-msg',data);
 }
-var timer = setInterval(update,1500);
+
+
+});
+
+
 
 http.listen(PORT, () => { /////local host
     console.log("Server started on port " + PORT);
 });
-
-// http.listen(3000, '104.131.1.90');
-// console.log('Server running at http://104.131.1.90:3000');
